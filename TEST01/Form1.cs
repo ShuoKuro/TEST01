402c9e2cb52e54b25afc96bd46fbb0cf11243e8d
Li <gjo4us06c961115@gmail.com>
2020-10-08 20:33:47 +0800

    加入專案檔案。

---
+++ TEST01/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TEST01
+{
+    public partial class Form1 : Form
+    {
+        int add(int a, int b,int c)
+        {
+            return a + b + c;
+        }
+        int sub(int a, int b)
+        {
+            return a - b;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_GO_Click(object sender, EventArgs e)
+        {
+            int sum = 0;
+            int a = 5;
+            int b = 4;
+            int c = 0;
+            sum = add(a, b, c);
+
+            Console.WriteLine("add()=" + sum);
+        }
+
+    }
+}
